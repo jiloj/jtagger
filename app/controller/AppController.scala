d2a70fea049edc30fa5e6271dca18d8508b27962
Matias Grioni <matgrioni@gmail.com>
2019-04-08 22:57:57 -0700

    Remove test function.

--- app/controller/AppController.scala
+++ app/controller/AppController.scala
@@ -20,8 +20,6 @@ class AppController @Inject()(appDAO: AppDAO, config: Configuration, cc: Control
 ) extends AbstractController(cc) {
   private val logger = Logger("jtagger")
 
-  def f(a: Int) = 10
-
   /**
     * Tags a given question.
     *
